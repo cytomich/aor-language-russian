0ce89c43a93164f3422a03591d0031b944ebce93
Ilya Akulenko <cytomich@gmail.com>
2017-02-22 20:44:06 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# Russian Translations for Admin-on-rest
+
+French translations for [admin-on-rest](https://github.com/marmelab/admin-on-rest), the frontend framework for building admin applications on top of REST services.
+
+## Installation
+
+```sh
+npm install --save aor-language-russian
+```
+
+## Usage
+
+```js
+import russianMessages from 'aor-language-russian';
+
+const messages = {
+    'ru': russianMessages,
+};
+
+<Admin locale="ru" messages={messages}>
+  ...
+</Admin>
+```
+
+## License
+
+This translation is licensed under the [MIT Licence](LICENSE).
---
+++ index.js
@@ -0,0 +1,63 @@
+module.exports = {
+    aor: {
+        action: {
+            delete: 'Удалить',
+            show: 'Просмотр',
+            list: 'Список',
+            save: 'Сохранить',
+            create: 'Создать',
+            edit: 'Редактировать',
+            cancel: 'Отмена',
+            refresh: 'Обновить',
+            add_filter: 'Добавить фильтр',
+            remove_filter: 'Убрать фильтр',
+        },
+        boolean: {
+            true: 'Да',
+            false: 'Нет',
+        },
+        page: {
+            list: 'Список %{name}',
+            edit: '%{name} #%{id}',
+            show: '%{name} #%{id}',
+            create: 'Создать %{name}',
+            delete: 'Удалить %{name} #%{id}',
+        },
+        input: {
+            image: {
+                upload_several: 'Перетащите файлы сюда или нажмите для выбора.',
+                upload_single: 'Перетащите файл сюда или нажмите для выбора.',
+            },
+        },
+        message: {
+            yes: 'Да',
+            no: 'Нет',
+            are_you_sure: 'Вы уверены?',
+            about: 'Справка',
+        },
+        navigation: {
+            page_out_of_boundaries: 'Страница %{page} вне границ',
+            page_out_from_end: 'Невозможно переместиться дальше последней страницы',
+            page_out_from_begin: 'Невозможно переместиться перед первой страницей',
+            page_range_info: '%{offsetBegin}-%{offsetEnd} из %{total}',
+            next: 'Следующая',
+            prev: 'Предыдущая',
+        },
+        auth: {
+            username: 'Имя пользователя',
+            password: 'Пароль',
+            sign_in: 'Войти',
+            sign_in_error: 'Аутентификация не успешна, попробуйте снова',
+            logout: 'Выйти',
+        },
+        notification: {
+            updated: 'Элемент обновлен',
+            created: 'Элемент создан',
+            deleted: 'Элемент удален',
+            item_doesnt_exist: 'Элемент не существует',
+        },
+        validation: {
+            required: 'Обязательно для заполнения',
+        },
+    },
+};
